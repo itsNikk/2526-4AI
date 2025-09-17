1f6c8f5cd3be0b75be7428f1e4169af5fdfe99e8
iTzNikk <nicolo.buganza@tiscali.it>
2025-09-17 16:08:38 +0200

    Create es2.sh

---
+++ PrimiEserciziGit/es2.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+cd $HOME/Desktop
+
+#Rimuovo eventuali vecchi repo così da iniziare da nuovo.
+#Git init se trova già una cartella con un determinato nome non riparte da zero.
+rm -rf secretRepo2
+git init secretRepo2
+
+cd secretRepo2
+
+echo "Non guardare!" > segreto.txt
+git add segreto.txt
+git commit -m "Added secret :)"
+
+
+if [ -f segreto.txt ]
+then
+        git rm segreto.txt
+        git commit -m "Segreto rimosso"
+else
+        echo "Nessun segreto trovato"
+fi
