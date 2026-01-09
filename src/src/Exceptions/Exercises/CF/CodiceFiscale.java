bf61054811c3b6d3c03a4a65c3398b9cd6b76173
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-01-09 11:10:50 +0100

    Update CodiceFiscale.java

--- src/src/Exceptions/Exercises/CF/CodiceFiscale.java
+++ src/src/Exceptions/Exercises/CF/CodiceFiscale.java
@@ -9,27 +9,25 @@ public class CodiceFiscale {
 
     private final int MAX_LENGTH = 16;
 
-
-    //Implementate questo esercizio sennza usare isLetterOrDigit oppure is Uppercase
+    //Implementate questo esercizio senza usare isLetterOrDigit oppure isUppercase
     public CodiceFiscale(String cf) throws CodiceFiscaleNonValidoException {
         if (cf.length() != MAX_LENGTH) throw new CodiceFiscaleNonValidoException("CF troppo lungo");
 
         for (int i = 0; i < cf.length(); i++) {
             if (!Character.isLetterOrDigit(cf.charAt(i)))
-                throw new CodiceFiscaleNonValidoException("cf non alfanumerico");
+                throw new CodiceFiscaleNonValidoException("CF non alfanumerico");
         }
 
         if (!OnlyCFStaticMethods.isCharUpperCase(cf.charAt(cf.length() - 1))) {
             throw new CodiceFiscaleNonValidoException("CTRL CHAR non valido");
         }
 
-
         this.cod = cf;
         /*
          * 1) SPLIT + FOR
          * 2) charAt + for
          * 3) for/foreach + ASCII
-         * 4) due cicli + alfabeto (occhioa due for, è sempre 16)
+         * 4) due cicli + alfabeto (occhio a due for, è sempre 16)
          */
     }
 
