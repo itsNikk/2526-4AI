3bd9fca677e2915b1a6612a8ac0e43c00bdd572b
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-01-16 09:41:27 +0100

    Update Main.java

--- src/src/Exceptions/Exercises/Pandora/Main.java
+++ src/src/Exceptions/Exercises/Pandora/Main.java
@@ -1,8 +1,23 @@
 package Exceptions.Exercises.Pandora;
 
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        Random r = new Random();
+        final double THRESHOLD = 0.1;
+        for (int i = 0; i < 100000; i++) {
+            double val = r.nextDouble(); //[0.0, 1.0])
+
+            System.out.println(i + " : " + val);
+            if (val <= THRESHOLD) {
+                throw new RuntimeException("BOOM");
+            }
+
+
+            //Sys call -PCB
+            Thread.sleep(1000);
+        }
     }
 }
