fbea8dc80cafca17eb4aef840607f3fc052afe68
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-02-06 11:21:30 +0100

    Update Atleta.java

--- Multithreading/src/Intro/Runners/Atleta.java
+++ Multithreading/src/Intro/Runners/Atleta.java
@@ -5,10 +5,12 @@ public class Atleta extends Thread {
     private long startTime;
     private long endTime;
     private boolean disqualified;
+    private boolean raceCompleted;
 
     public Atleta(String nome) {
         super(nome); //setName(name);
         this.disqualified = false;
+        this.raceCompleted = false;
     }
 
     @Override
@@ -20,13 +22,13 @@ public class Atleta extends Thread {
                 System.out.println(getName() + ": Step " + (i + 1));
                 sleep(300 + (int) (Math.random() * 500));
             } catch (InterruptedException e) {
-                //che si fa quando qualcuno mi interrompe?
+                //che si fa quando qualcuno chiama interrupt su un qualunque Thread Atleta?
                 disqualified = true;
                 System.out.println(getName() + " ritirato / qualificato");
                 break;
             }
         }
-
+        raceCompleted = true;
         endTime = System.currentTimeMillis();
     }
 
@@ -36,5 +38,8 @@ public class Atleta extends Thread {
 
     public boolean getDisqualified() {
         return disqualified;
+    }
+    public boolean getRaceCompleted() {
+        return raceCompleted;
     }
 }
