5c099d22534bda063815630dffce28051b7c2227
iTzNikk <nicolo.buganza@tiscali.it>
2026-03-11 16:10:56 +0100

    Init alchemist solution

---
+++ src/src/Exceptions/verifica27022026/Alchimist/JungleHazardException.java
@@ -0,0 +1,7 @@
+package Exceptions.verifica27022026.Alchimist;
+
+public class JungleHazardException extends Exception {
+    public JungleHazardException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/src/Exceptions/verifica27022026/Alchimist/OutOfEnergyException.java
@@ -0,0 +1,7 @@
+package Exceptions.verifica27022026.Alchimist;
+
+public class OutOfEnergyException extends RuntimeException {
+    public OutOfEnergyException(String msg) {
+        super(msg);
+    }
+}
