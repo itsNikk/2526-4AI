44da9d307e480d97448e1ecfd91573bbebc735d9
iTzNikk <nicolo.buganza@tiscali.it>
2026-04-27 22:08:40 +0200

    Join con cleanup

---
+++ Multithreading/src/Exercises/JoinAndCleanup/ElaborationEntity.java
@@ -0,0 +1,27 @@
+package Exercises.JoinAndCleanup;
+
+import java.util.Random;
+
+public class ElaborationEntity extends Thread {
+
+    private final Random random;
+
+    public ElaborationEntity(String nome) {
+        super(nome);
+        this.random = new Random();
+    }
+
+    @Override
+    public void run() {
+        try {
+            int durata = 3000 + random.nextInt(10000);
+            System.out.println("[" + getName() + "] avviato (durata prevista: " + durata + "ms)");
+            Thread.sleep(durata);
+            System.out.println("[" + getName() + "] completato.");
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("[" + getName() + "] interrotto prima del completamento.");
+        }
+    }
+}
