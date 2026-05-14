0c3e09fbc6e6e29cb153eaa9319f4e75eecceb0c
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-05-14 11:03:33 +0200

    Update Main.java

--- Multithreading/src/sportello13052026/Main.java
+++ Multithreading/src/sportello13052026/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException{
         //CTRL+ALT+T
         Magazzino magazzino = new Magazzino(5);
-        // create 20 produttori
+        // create 20 produttodri
         ArrayList<Fornitore> f = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             //f.add(new Fornitore(magazzino));
